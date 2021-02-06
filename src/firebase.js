e1507f96992cf69a05d470e158c7cb7ae7c641fa
lunariweb <lunaristudiosglobal@gmail.com>
2021-02-07 01:25:56 +0530

    initial release

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDAsJ71dALK7QhkMqZyB6ErUj9fndBCE1E",
+    authDomain: "clone-dfa99.firebaseapp.com",
+    projectId: "clone-dfa99",
+    storageBucket: "clone-dfa99.appspot.com",
+    messagingSenderId: "264715681148",
+    appId: "1:264715681148:web:38e36c0a7bba032c7b38f9",
+    measurementId: "G-QHBN21B2EM"
+  };
